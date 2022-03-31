0f7205684a50726a64637568f4f220cdb76f7889
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-04-01 01:55:44 +0530

    Time: 68 ms (96.47%), Space: 37.2 MB (47.51%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -6,6 +6,8 @@ public:
         int count = 0, sum = 0;
         for (auto n: nums) {
             sum += n;
+              if (seen.end() != seen.find(sum - k))
+          
             count += seen[sum - k];
             seen[sum]++;
         }
