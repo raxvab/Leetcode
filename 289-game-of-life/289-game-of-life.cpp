38d4cd5b3209b17ec88346a7050a9c882e136734
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-04-12 13:46:45 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (58.55%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -5,21 +5,12 @@ public:
     void gameOfLife(vector<vector<int>>& a) {
         int n=a.size();
         int m= a[0].size();
-      //= int a[n+2][m+2]={0};
+      
         for(int i=0;i<n;i++)
             for(int j=0;j<m;j++)
             {
                  int sum=0;
-//         sum= a[i-1][j-1]+a[i-1][j-1]+a[i-1][j+1];
-//                 sum+= a[i][j-1]+a[i][j+1];
 
-//                 sum+= a[i+1][j-1]+a[i+1][j-1]+a[i+1][j+1];
-// if((sum<2||sum>3 ) and a[i][j]==1)
-//     a[i][j]=0;
-//       if((sum==3 ) and a[i][j]==0)
-//    a[i][j]=1;
-//         if((sum==3||sum==2 ) and a[i][j]==1)
-//    a[i][j]=1;
                 ////////////////////////////////////////
                 if(i>0 and a[i-1][j]>=1) sum++;
                 if(i<n-1 and a[i+1][j]>=1) sum++;
@@ -34,9 +25,8 @@ public:
                 if(a[i][j]==1 and (sum<2||sum>3)) a[i][j]=2;
                 if(a[i][j]==0 and sum==3 ) a[i][j]=-1;
                 
-                
-                
             }
+        
          for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(a[i][j]==2) a[i][j]=0;
