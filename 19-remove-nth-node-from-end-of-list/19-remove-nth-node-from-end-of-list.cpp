2fdbec849c444874ebe94efe202d4b1d759ba14e
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-04-14 00:48:48 +0530

    Time: 7 ms (49.12%), Space: 10.8 MB (32.88%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,34 +11,34 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-//         if (head == NULL||head->next==NULL)
-// return head;
-       ListNode* d=head;
-    int c=1;
-    while(d->next!=NULL)
+ // optimal two pointer approach baabeeeyyyeeehhh
+
+    ListNode*silo = head; //phast=1;
+   ListNode*phast = head;
+   for (int i = 0; i <n; i++)
+       {
+    if (i==n-1&&phast->next==NULL)
     {
-        d=d->next;
-        c++;
+        // if the length of linkedlist is equal to the given position form the end point
+        //  then that means it wants us to delete the head itself
+        ListNode *t = head;
+        head = head->next;
+        delete t;
+        return head;
     }
-    if(c==n)
+     phast = phast->next;
+    }
+    //here the main logic is that the fast pointer will reach end
+    // and eventually maintain the gap of n value to the slow pointer
+    // then we'll simply delete slow->next
+    while (phast && phast->next != NULL)
     {
-//if the length of linkedlist is equal to the given position form the end point
-// then that means it wants us to delete the head itself        
- ListNode* t= head;
-head=head->next;
-delete t;
-return head ;
+        phast = phast->next;
+        silo = silo->next;
     }
-    c=c-n;
-    int c2=1;
- ListNode* t1=head;
-while(t1->next!=NULL&&c2<c){
-    t1=t1->next;
-c2++;
-}
- ListNode* todelete=t1->next;
-t1->next=t1->next->next;
-delete todelete;
-return head;
+ListNode*todelete = silo->next;
+    silo->next = silo->next->next;
+    delete todelete;
+    return head;
     }
 };
