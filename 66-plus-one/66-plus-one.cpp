e37696e57e23ae80bc93175b69d43c7bb425456d
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-04-14 14:46:35 +0530

    Time: 3 ms (60.38%), Space: 8.8 MB (61.70%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        // may be it become a loop of ADDITION IF the ending digit is 9
+        
         for(int i = digits.size() - 1;i >= 0;i--){
             if(digits[i] != 9){
                 digits[i]++;
@@ -9,6 +11,9 @@ public:
             else
                 digits[i] = 0;
         }
+        
+        // learned a new thing the how to insert an element in the beginning
+        // here if in case qall the digits somehow added upto 9 and a carry was taken ecverytime then we insert a 1 in the beginning
          digits.insert(digits.begin(),1);
         return digits;
        
