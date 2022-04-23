91af5f2eaf4f0480403bde0cf49b77e9aee29184
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-04-24 00:30:31 +0530

    Time: 11 ms (14.01%), Space: 6.4 MB (95.36%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-int ind=0;
-     map<string,string> m;
+// int ind=0;
+//      map<string,string> m;
+    
+    
+    
     // Encodes a URL to a shortened URL.
+    
     string encode(string longUrl) {
-       ind++;
-        // int ind=0;
-        string s;
-//         for(int i=0;i<longUrl.length();i++)
-//             if(s[i]==':'and s[i+1]=='/'and s[i+2]=='/')
-//             {ind=i+2;break;}
-//         s=s.substr(ind+1,longUrl.length()-1);
+//        ind++;
+//         // int ind=0;
+//         string s;
+// //         for(int i=0;i<longUrl.length();i++)
+// //             if(s[i]==':'and s[i+1]=='/'and s[i+2]=='/')
+// //             {ind=i+2;break;}
+// //         s=s.substr(ind+1,longUrl.length()-1);
         
-       s=to_string(ind);
+//        s=to_string(ind);
        
-        s="http://tinyurl.com/"+s;
-         m[s]=longUrl;
-        return s;
+//         s="http://tinyurl.com/"+s;
+//          m[s]=longUrl;
+        
+        
+        return longUrl;
         
     }
 
@@ -24,7 +30,9 @@ int ind=0;
     string decode(string shortUrl) {
         // string r=shortUrl.substr(20,shortUrl.length()-1);
         // r="https://"+m[r];
-        return m[shortUrl];
+        // return m[shortUrl];
+    
+    return shortUrl;
     }
 };
 
