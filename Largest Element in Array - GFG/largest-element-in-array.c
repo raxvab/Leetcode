5456b1e8ab26839fe0e6a9eceeba25d3a0a4a687
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-04-25 21:51:06 +0530

    Added solution - LeetHub

---
+++ Largest Element in Array - GFG/largest-element-in-array.c
@@ -0,0 +1,29 @@
+// { Driver Code Starts
+#include <stdio.h>
+
+int largest(int arr[], int n);
+
+int main() {
+    int arr[1000];
+    int i, t, n;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        for (i = 0; i < n; i++) scanf("%d", &arr[i]);
+        printf("%d", largest(arr, n));
+        printf("\n");
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+// C function to find maximum in arr[] of size n
+int largest(int arr[], int n) {
+    int maxx=-1;
+    for(int i=0;i<n;i++)
+    if(arr[i]>maxx)
+    maxx=arr[i];
+    
+    return maxx;
+}
