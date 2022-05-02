3f2e64434ac54fd77ac3838e0f43a8aa1ddee4f3
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-05-03 00:04:32 +0530

    Time: 145 ms (29.06%), Space: 46.7 MB (80.13%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,9 +2,13 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         // space ciomplexity hike    
-        unordered_set<int> s(nums.begin(), nums.end());
+//         unordered_set<int> s(nums.begin(), nums.end());
      
-        return (s.size() != nums.size());
+//         return (s.size() != nums.size());
 
+      sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++)
+            if (nums[i] == nums[i - 1]) return true;
+        return false;
     }
 };
