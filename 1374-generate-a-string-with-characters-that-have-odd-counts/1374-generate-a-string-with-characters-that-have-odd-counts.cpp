35d7f331db1a8498d4d26f942a3f1e186b72d3e0
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-05-04 11:56:05 +0530

    Time: 6 ms (15.44%), Space: 6.5 MB (17.58%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     string generateTheString(int n) {
-        string s="";
-        if(n<=26)
-        {
-            while(n--)
-                s+=n+'a';
-            return s;
-        }
-        else
-        {n--;
-         if(n%2==1)
-          { while(n--)
-               s+='a';
-            s+='b';}
-         else
-         { n++;
-             while(n--)
-              s+='a';   
-         }
-            return s;
-            }
+//         string s="";
+//         if(n<=26)
+//         {
+//             while(n--)
+//                 s+=n+'a';
+//             return s;
+//         }
+//         else
+//         {n--;
+//          if(n%2==1)
+//           { while(n--)
+//                s+='a';
+//             s+='b';}
+//          else
+//          { n++;
+//              while(n--)
+//               s+='a';   
+//          }
+//             return s;
+//             }
       
-        return s;
+//         return s;
         
+        // thanks to discuss
+             return n % 2 ? string(n, 'a') : string(n - 1, 'a') + 'b';
     }
 };
