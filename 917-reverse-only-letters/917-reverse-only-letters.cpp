c4b07ed32d07076e2b5ac14a27ec0f29c4115279
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-05-05 15:31:16 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (70.17%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.cpp
+++ 917-reverse-only-letters/917-reverse-only-letters.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-      stack<char>a;
-        for(auto i:s)
-            if(isalpha(i))
-            a.push(i);
-        // reverse(a.begin(),a.end());
-        // cout<<a;
-         int k=0;
-        for(int i=0;i<s.length();i++)
-            if(isalpha(s[i])){
-         s[i]=a.top();
-                a.pop();
-         k++;}
+      // stack<char>a;
+      //   for(auto i:s)
+      //       if(isalpha(i))
+      //       a.push(i);
+      //   // reverse(a.begin(),a.end());
+      //   // cout<<a;
+      //    int k=0;
+      //   for(int i=0;i<s.length();i++)
+      //       if(isalpha(s[i])){
+      //    s[i]=a.top();
+      //           a.pop();
+      //    k++;}
+         int l = 0, r = s.length() - 1;
+        while (l < r) {
+            while (l < r && !isalpha(s[l]))
+                ++l;
+            while (l < r && !isalpha(s[r])) 
+                --r; 
+            swap(s[l++], s[r--]);
+        }
+         
         return s;
     }
 };
