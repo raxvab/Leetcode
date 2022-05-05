f6a08908c284926cd9a5f1397f43771c69ee937f
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-05-05 14:59:21 +0530

    Time: 0 ms (100.00%), Space: 6 MB (93.06%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<s.length();i++)
 		{	if(i+2<s.length()&&s[i + 2] == '#'){
 				int t = (s[i] - '0') * 10 + (s[i + 1] -  '0');
-				ans += char(t + 96);  // char(t + 'a' - 1)
+				ans += char(t + 96); 
 				i+=2;
 			}
 			else{
