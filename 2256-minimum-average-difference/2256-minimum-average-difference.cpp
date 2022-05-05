5b3e5774ea5cf3b0406d660b353e7dd58e4655b0
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-05-06 03:38:39 +0530

    Time: 173 ms (41.69%), Space: 84.6 MB (43.74%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
+       
         int n=nums.size();
 vector<long long int> v(n);
-        v[0]=nums[0];
+       v[0]=nums[0];
         for(int i=1;i<n;i++)
         {
           v[i]=nums[i]+v[i-1];
@@ -15,23 +16,23 @@ long long int m=INT_MAX;
      
             if(i!=n-1)
             {
-              
+               
                   if(m>abs((v[i]/(i+1))-((v[n-1]-v[i])/(n-i-1))))
-                  {  m=abs((v[i]/(i+1))-((v[n-1]-v[i])/(n-i-1)));
-                   ind=i;
-                  }
+                 {  m=abs((v[i]/(i+1))-((v[n-1]-v[i])/(n-i-1)));
+                   ind=i;}
+       
             }
             else if(m>(v[i]/(i+1)))
                    {
                     m=v[i]/(i+1);
                     ind =i;
                 }
-           
+          
         
         }
         
        
-   
+     //  cout<<m;
         return ind;
     }
 };
