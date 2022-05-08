d0ae9455fc4b73570da160fc26e8f228a3654135
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-05-08 18:24:23 +0530

    Time: 57 ms (40.96%), Space: 21.8 MB (22.03%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
-    bool check(string &s,string &t){
-            int a1[26]={0};
-                int a2[26]={0};
-      if(s.length()!=t.length())
-         return false;
-        
-for(auto i:s)
-    a1[i-'a']++;
-for(auto i:t)
-    a2[i-'a']++;
-        
-        for(int i=0;i<26;i++)
-            if(a1[i]!=a2[i])
-                return false;
-        return true;}
-    
+   
     
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> m;
