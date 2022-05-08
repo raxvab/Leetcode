72e924e04b6234da04b25754e86284e00ce03d37
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-05-08 21:59:37 +0530

    Time: 3 ms (86.26%), Space: 8.6 MB (45.75%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+    vector<string> findAndReplacePattern(vector<string>& w, string pattern) {
          vector<string> ans;
-        for(int i=0;i<words.size();i++){
+        for(int i=0;i<w.size();i++){
             int f = 1;
             map<char,int> m1,m2;
-            for(int j=0;j<words[i].size();j++){
-                if(m1.find(pattern[j])!=m1.end() && m1[pattern[j]]!=words[i][j]){
+            for(int j=0;j<w[i].size();j++){
+                if(m1.find(pattern[j])!=m1.end() && m1[pattern[j]]!=w[i][j]){
                     f = 0;
                     break;
                 }
-                else if(m2.find(words[i][j])!=m2.end() && m2[words[i][j]]!=pattern[j]){
+                else if(m2.find(w[i][j])!=m2.end() && m2[w[i][j]]!=pattern[j]){
                     f = 0;
                     break;
                 }
                 else{
-                    m1[pattern[j]] = words[i][j];
-                    m2[words[i][j]] = pattern[j];
+                    m1[pattern[j]] = w[i][j];
+                    m2[w[i][j]] = pattern[j];
                 }
             }
             if(f){
-                ans.push_back(words[i]);
+                ans.push_back(w[i]);
             }
         }
         return ans;
