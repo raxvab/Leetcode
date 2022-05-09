c6a1b5e8ec5902fc67e5fe26895ef1334fe82e46
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-05-10 00:29:07 +0530

    Time: 11 ms (53.92%), Space: 11.2 MB (21.52%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -1,11 +1,11 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *   int val;
+ *   ListNode * next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
 class Solution
@@ -14,27 +14,28 @@ class Solution
        	//youtube help https://www.youtube.com/watch?v=R6-PnHODewY
         ListNode* deleteDuplicates(ListNode *head)
         {
-            // if (head == NULL || head->next == NULL)
-            //     return head;
+            // So the basic condition check is here
+           	if (head == NULL || head->next == NULL)
+           	    return head;
 
-            ListNode *dummy =new ListNode(0,head);
+            ListNode *dummy = new ListNode(0, head);
             ListNode *prev = dummy;
 
             while (head != NULL)
             {
-                if (head->next!=NULL and head->val == head->next->val)
+                if (head->next != NULL and head->val == head->next->val)
 
                 {
-                    while (head->next!=NULL and head->val == head->next->val)
-                    head = head->next;
-                    
+                    while (head->next != NULL and head->val == head->next->val)
+                        head = head->next;
+
                     prev->next = head->next;
                 }
                 else
-                prev = prev->next;
+                    prev = prev->next;
 
                 head = head->next;
             }
-          return dummy->next;
+            return dummy->next;
         }
 };
