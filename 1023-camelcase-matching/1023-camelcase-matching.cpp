53343511438fc455fce58f8ad4447436ac9fd49c
vaibhav shukla <63441521+raxvab@users.noreply.github.com>
2022-05-09 22:38:52 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (6.51%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.cpp
+++ 1023-camelcase-matching/1023-camelcase-matching.cpp
@@ -1,32 +1,47 @@
 class Solution
 {
-     bool check(string s, string t){
-        int j=0;
+    // trying to comment the code and by submitting aghain it will push and reflect changes in my repo as well
+    // here i have created a check function which basically does is compares the pattern parsed and the substring of the original input string
+    
+    bool check(string s, string t)
+    {
+        
+  // intialize a new dummy string named str
         string str;
-        for(int i=0;i<s.size();i++){
-            if(isupper(s[i])){
-                    str+=s[i];
-                if(s[i]==t[j])j++;
+        // In this loop we check weather the newly built string's characters matches the pattern with the specific uppercase and lowercase comparisons 
+        int j = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (isupper(s[i]))
+            {
+                str += s[i];
+                // here after addin the current char to the dummy striung what we're tryinig to do is  , comparing the chars with pattern string
+                if (s[i] == t[j]) 
+                    j++;
+                
             }
-            else if(islower(s[i])&&s[i]==t[j]){
-                str+=s[i];
+            // similar in lowercase letter's case as well
+            else if (islower(s[i]) && s[i] == t[j])
+            {
+                str += s[i];
                 j++;
             }
         }
-        if(str==t)return true;
+        // lastly we're trying to check  weather the strign generated matches the pattern or not
+        if (str == t) return true;
         return false;
     }
     public:
         vector<bool> camelMatch(vector<string> &queries, string p)
         {
-           vector<bool>v;
-        for(auto i:queries){
-            if(check(i,p))
-                v.push_back(true);
-            else v.push_back(false);
+            vector<bool> v;
+            // we're parsing the queries and the pattern to check if the condition mentioned holds
+            for (auto i: queries)
+            {
+                if (check(i, p))
+                    v.push_back(true);
+                else v.push_back(false);
+            }
+            return v;
         }
-        return v;
-    }
-   
-        
 };
