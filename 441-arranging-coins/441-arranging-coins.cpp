d879454befebf9f89ca95423c78f093d6d8f6e9a
Vaibhav Shukla <63441521+raxvab@users.noreply.github.com>
2022-09-05 23:03:42 +0530

    Time: 6 ms (60.82%), Space: 5.8 MB (69.84%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long  l=1,h=n;
+      long long res=INT_MIN;
+      while(l<=h)
+      {
+        long long int mid=l+(h-l)/2;
+        
+        long long pod=mid*mid;
+        pod+=mid;
+        pod/=2;
+        
+        if(pod<=n)
+          res=max(res,mid);
+        if( pod>n)
+          h=mid-1;
+        else
+          l=mid+1;
+        
+      }
+      return  (int)res;
+    }
+};
